bbb17cc0447bcb3820079764d4fedca854e019f1
ikuya126 <ikuya126@icloud.com>
2024-08-17 13:16:24 +0900

    メール送信機能追加

---
+++ src/resources/views/mail/email.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app2')
+
+@section('title')
+メール受信
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/email.css') }}">
+@endsection
+
+@section('content')
+<span>会員登録完了のお知らせ</span>
+@endsection
