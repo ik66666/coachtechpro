32834b8d885b3fb45ed7b4cdc6869482133545e6
ikuya <ikuya126@gmail.com>
2024-09-05 22:10:46 +0900

    環境設定

--- src/config/database.php
+++ src/config/database.php
@@ -83,25 +83,20 @@ return [
             ]) : [],
         ],
 
-        'circle_test' => [
+        'circle_testing' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => 'demo_test',
-            'username' => 'ubuntu',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'circle_test',
+            'username' => 'root',
             'password' => '',
-            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
+    ],
 
         'mysql_production' => [
             'driver' => 'mysql',
